f9d80cead22994755285064ef462ac6e7d4cfa4a
Bruce Wu <rs665507@gmail.com.tw>
2021-06-27 15:42:35 +0800

    1nd

---
+++ TeseArray.java
@@ -0,0 +1,24 @@
+package xxx;
+
+public class TeseArray {
+
+	public static void main(String[] args) {
+		int[][] x = {
+				{13, 8, 17},
+				{13, 24, 26},
+				{10, 5 ,27}
+		};
+		
+		int[][] y = {
+				{1, 23, 21},
+				{2, 9, 27},
+				{13, 17, 20}
+		};
+		
+		int[][] z = new int[3][3];
+		
+		System.out.println() ;
+		
+		
+	}
+}
